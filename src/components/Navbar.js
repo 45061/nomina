007c38702d31cb8b3ee9e5a33d124f13f51f759a
Anderson <andeersonr@gmail.com>
2023-02-27 21:33:42 -0500

    protection route managment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,25 @@
 import Link from "next/link";
+import { useSelector } from "react-redux";
 import styles from "../styles/components/Navbar.module.scss";
 
 import MenuNavbar from "./MenuNavbar";
 
 export default function Navbar() {
+  const { isAuth } = useSelector((state) => state.authReducer);
+
   return (
     <div className={styles.container}>
       <Link href="/" prefetch={false}>
         <h2>Nomina App</h2>
       </Link>
       <div className={styles.container__nav}>
-        <Link href="/management" prefetch={false}>
-          <h3>Gestión</h3>
-        </Link>
+        {isAuth ? (
+          <Link href="/management" prefetch={false}>
+            <h3>Gestión</h3>
+          </Link>
+        ) : (
+          <div></div>
+        )}
         <h3>Informe</h3>
         <MenuNavbar />
       </div>
